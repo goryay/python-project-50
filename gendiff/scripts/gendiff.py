5a9fe0d860902cfb7042c4325b79497a184c03ee
Ilya <iam.goryn@gmail.com>
2023-09-19 16:58:04 +0300

    file editing gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
-from gendiff.get_diff import generate_diff
-from gendiff.cli import parse
+
+
+from gendiff.cli import make_parser
+from gendiff.gendiff import generate_diff
 
 
 def main():
-    path_to_file1, path_to_file2, formatter = parse()
-    diff_formatted = generate_diff(path_to_file1, path_to_file2, formatter)
-    print(diff_formatted)
+    args = make_parser().parse_args()
+
+    print(generate_diff(args.path_file1, args.path_file2, args.format))
+
+    return
 
 
 if __name__ == '__main__':
